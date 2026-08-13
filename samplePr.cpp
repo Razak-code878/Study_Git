088d4c2c7c1e55e31e91f14a97e50ce320f1fd43
Razac <furferkarim@gmail.com>
2026-08-13 20:11:35 +0300

    Repaired mistake with include and added by init

--- samplePr.cpp
+++ samplePr.cpp
@@ -1,10 +1,13 @@
-#include <bits\stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long 
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
+
+    // init
+    ll n; cin >> n;
 
     return 0;
 }
