e2fef38b54519b4a7d50e62da043edda1efda10e
Razac <furferkarim@gmail.com>
2026-08-14 10:48:03 +0300

    Added a new task

---
+++ bcteria.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll bitsCount(ll x, ll system){
+    ll res = 0;
+    
+    while (x > 0){
+        res += x%system;
+        x /= system;
+    }
+    
+    return res;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    
+    ll x; cin >> x;
+    
+    cout << bitsCount(x, 2);
+    
+    return 0;
+}
